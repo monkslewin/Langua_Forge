4307be36e5097ee1dbc587df56fe2a97d5c905cc
Finn Monks-Lewin <finnml04@icloud.com>
2026-07-22 16:56:32 +1000

    more database changes

---
+++ LanguaForge.API/Migrations/20260722065624_EditConjTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanguaForge.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditConjTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Mood",
+                table: "Conjugation",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Number",
+                table: "Conjugation",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mood",
+                table: "Conjugation");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Conjugation");
+        }
+    }
+}
--- LanguaForge.API/Models/Conjugation.cs
+++ LanguaForge.API/Models/Conjugation.cs
@@ -4,13 +4,26 @@ public class Conjugation
 {
     public int Id { get; set; }
 
-    public string Tense { get; set; } = "";
 
-    public string Person { get; set; } = "";
-
-    public string Form { get; set; } = "";
-
+    // Foreign key
     public int VerbId { get; set; }
-    
+
+
+    // Navigation property
     public Verb Verb { get; set; } = null!;
+
+
+    public string Tense { get; set; } = string.Empty;
+
+
+    public string Mood { get; set; } = string.Empty;
+
+
+    public string Person { get; set; } = string.Empty;
+
+
+    public string Number { get; set; } = string.Empty;
+
+
+    public string Form { get; set; } = string.Empty;
 }
